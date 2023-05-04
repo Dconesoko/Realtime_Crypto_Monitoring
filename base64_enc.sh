0884c2bc1bcd1240030547543c0ffe386badeb96
Nana Bediako <dconesoko@gmail.com>
2023-05-04 15:31:58 -0230

    added secrets.yaml file

---
+++ base64_enc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Read the content of the .env file line by line
+cat .env | while IFS= read -r line; do
+
+  # Split the line into key and value parts using '=' as the delimiter
+  key=$(echo "$line" | cut -d '=' -f 1)
+  value=$(echo "$line" | cut -d '=' -f 2-)
+
+  # Encode the value using base64
+  encoded_value=$(echo "$value" | base64)
+
+  # Add the key and encoded value to the secret.yaml file
+  echo "  $key: $encoded_value" >> ./k8s/postgres/secrets.yaml
+
+done
