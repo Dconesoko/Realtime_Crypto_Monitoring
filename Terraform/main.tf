9acd1260e394c6cc52ceca6b91cee830976972f7
Nana Bediako <dconesoko@gmail.com>
2023-05-10 23:09:40 -0230

    added new dir to /var/lib/postgresql/data

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "primary" {
   name     = "realtime-crypto"
   location = "us-central1-a"
 
-  initial_node_count = 3
+  initial_node_count = 2
 
   node_config {
     preemptible  = true
